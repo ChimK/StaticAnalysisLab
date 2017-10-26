3743d7e5598457f743793d2f939df747ec180263
Neil Walkinshaw <nw91@le.ac.uk>
2017-10-26 11:43:09 +0100

    Possible lab solution

--- src/main/java/structuralAnalysis/ClassDiagram.java
+++ src/main/java/structuralAnalysis/ClassDiagram.java
@@ -1,21 +1,26 @@
 package structuralAnalysis;
 
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * Created by neilwalkinshaw on 24/10/2017.
  */
 public class ClassDiagram {
 
-    /**
-     * Given a package name and a directory returns all classes within that directory
-     * @param directory
-     * @param pkgname
-     * @return Classes within Directory with package name
-     */
+    protected Map<String,String> inheritance;
+    protected Map<String,Set<String>> associations;
+
+    //include classes in the JDK etc? Can produce crowded diagrams.
+    protected boolean includeLibraryClasses = true;
+
+    protected Set<String> allClassNames;
+
     public static List<Class<?>> processDirectory(File directory, String pkgname) {
 
         ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
@@ -37,7 +42,9 @@ public class ClassDiagram {
 
 
             if (className != null) {
-                classes.add(loadClass(className));
+                Class loaded = loadClass(className);
+                if(loaded!=null)
+                    classes.add(loaded);
             }
 
             //If the file is a directory recursively class this method.
@@ -57,14 +64,94 @@ public class ClassDiagram {
         catch (ClassNotFoundException e) {
             throw new RuntimeException("Unexpected ClassNotFoundException loading class '" + className + "'");
         }
+        catch (Error e){
+            return null;
+        }
     }
 
 
-
-    public ClassDiagram(String root){
+    /**
+     * Instantiating the class will populate the inheritance and association relations.
+     * @param root
+     */
+    public ClassDiagram(String root, boolean includeLibs){
+        this.includeLibraryClasses = includeLibs;
         File dir = new File(root);
+
         List<Class<?>> classes = processDirectory(dir,"");
-        //Insert your lab code here.
+        inheritance = new HashMap<String, String>();
+        associations = new HashMap<String, Set<String>>();
+
+
+
+        allClassNames = new HashSet<String>();
+        for(Class cl : classes){
+            allClassNames.add(cl.getName());
+        }
+
+        for(Class cl : classes){
+            if(cl.isInterface())
+                continue;
+            inheritance.put(cl.getName(),cl.getSuperclass().getName());
+            Set<String> fields = new HashSet<String>();
+            for(Field fld : cl.getDeclaredFields()){
+                //Do not want to include associations to primitive types such as ints or doubles.
+                if(!fld.getType().isPrimitive()) {
+                    fields.add(fld.getType().getName());
+                }
+            }
+            associations.put(cl.getName(),fields);
+        }
+    }
+
+    /**
+     * Write out the class diagram to a specified file.
+     * @param target
+     */
+    public void writeDot(File target) throws IOException {
+        BufferedWriter fw = new BufferedWriter(new FileWriter(target));
+        StringBuffer dotGraph = new StringBuffer();
+        Collection<String> dotGraphClasses = new HashSet<String>(); //need this to specify that shape of each class should be a square.
+        dotGraph.append("digraph classDiagram{\n" +
+                "graph [splines=ortho]\n\n");
+
+        //Add inheritance relations
+        for(String childClass : inheritance.keySet()){
+            String from = "\""+childClass +"\"";
+            dotGraphClasses.add(from);
+            String to = "\""+inheritance.get(childClass)+"\"";
+            if(!includeLibraryClasses){
+                if(!allClassNames.contains(inheritance.get(childClass)))
+                    continue;
+            }
+            dotGraphClasses.add(to);
+            dotGraph.append(from+ " -> "+to+"[arrowhead = onormal];\n");
+        }
+
+        //Add associations
+        for(String cls : associations.keySet()){
+            Set<String> fields = associations.get(cls);
+            for(String field : fields) {
+                String from = "\""+cls +"\"";
+                dotGraphClasses.add(from);
+                String to = "\""+field+"\"";
+                if(!includeLibraryClasses){
+                    if(!allClassNames.contains(field))
+                        continue;
+                }
+                dotGraphClasses.add(to);
+                dotGraph.append(from + " -> " +to + "[arrowhead = diamond];\n");
+            }
+        }
+
+        for(String node : dotGraphClasses){
+            dotGraph.append(node+ "[shape = box];\n");
+        }
+
+        dotGraph.append("}");
+        fw.write(dotGraph.toString());
+        fw.flush();
+        fw.close();
     }
 
 
