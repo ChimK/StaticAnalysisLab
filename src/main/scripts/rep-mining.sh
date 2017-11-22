c657a1444f1df557c9023310abaaff3df8232b58
Neil Walkinshaw <nw91@le.ac.uk>
2017-11-22 08:01:58 +0000

    added comments and cleaned up rep-mining.she

--- src/main/scripts/rep-mining.sh
+++ src/main/scripts/rep-mining.sh
@@ -22,27 +22,35 @@ cd $REPO_DIR
 
 echo "Timestamp, Message, Committer, LOC" > $DATASTORE
 
+#Obtain an array of hash-codes for the previous versions.
 versions=($(git log master --no-walk --tags --pretty=format:"%h" -$NUM_VERSIONS ))
 
+#Obtain the number of elements in the array.
 num=${#versions[@]}
 
+#Iterate through the array of versions, and for each version do:
 for j in $(seq 1 $num); do
 
+    #Store the hash code of the current version as i
     i=${versions[$j]}
 
+    #Obtain the source code for the current version i by checking it out.
     git checkout -f $i
 
-    next=$(($j + 1))
-    nextver=${versions[$next]}
+    #Obtain the date that version i was committed (via git show)
+	  DATE=$(git show -s --format='%ct' $i)
 
-	DATE=$(git show -s --format='%ct' $i)
+    #Obtain the commit message for the current version (also via git show)
+    MESSAGE=$(git show -s --format='%B' $i | tr -d '[:punct:]\r\n')
 
-	MESSAGE=$(git show -s --format='%B' $i | tr -d '[:punct:]\r\n')
+    #Obtain the author ID for the current version (also via git show)
+    AUTHOR=$(git show -s --format='%an' $i)
 
-	AUTHOR=$(git show -s --format='%an' $i)
+    #Obtain the total LOC in the source directory.
+    #(This is where you can include whatever code you want to execute on the repository.)
+    LOC=$(find src -name '*.java' | xargs wc -l | awk {'print $1'} | tail -n1)
 
-	LOC=$(find src -name '*.java' | xargs wc -l | awk {'print $1'} | tail -n1)
-
-	echo "\"$DATE\", \"$MESSAGE\", \"$AUTHOR\", $LOC" >> $DATASTORE
+    #Pipe the various details that you have collected into the CSV file.
+    echo "\"$DATE\", \"$MESSAGE\", \"$AUTHOR\", $LOC" >> $DATASTORE
 
 done
